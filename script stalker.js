9213733f5ee80c5946fb72c904a988682b3f7cf7
RevathiElango <71167332+RevathiElango@users.noreply.github.com>
2020-10-01 22:34:22 +0530

    Add files via upload

---
+++ script stalker.js
@@ -0,0 +1,6 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your lastname?");
+var age = prompt("What is your age?");
+var fullName = firstName +" " + lastName
+console.log("Your full name is " + fullName);
+console.log("Your age " + age + "years old");
